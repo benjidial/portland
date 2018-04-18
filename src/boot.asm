2f519fc8cb9aa67d543bb032444366c3244eb877
benjidial <Benji3.141@gmail.com>
2018-04-18 14:48:00 -0400

    A hello world, a start

---
+++ src/boot.asm
@@ -0,0 +1,25 @@
+MAGIC equ 0x1BADB002
+
+section .multiboot
+align 4
+  dd MAGIC
+  dd 3
+  dd -MAGIC-3
+
+section .bss
+align 16
+stack_bottom:
+  resb 0x4000
+stack_top:
+ 
+section .text
+global _start:function (_end - _start)
+_start:
+  mov esp, stack_top
+
+  extern kernel_main
+  call kernel_main
+
+  cli
+  hlt
+_end:
