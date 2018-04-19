5a05f1d2a649488b5717b30d4db07940cb3f8ed4
benjidial <Benji3.141@gmail.com>
2018-04-18 21:03:01 -0400

    Curse you, precedence!

--- src/screen.c
+++ src/screen.c
@@ -12,14 +12,14 @@ void scr_init(void) {
 }
 
 void scr_clear(uint8_t color) {
-  uint16_t _ = color << 8 + 0x20;
+  uint16_t _ = ((uint16_t)color << 8) + 0x20;
   for (size_t i = 0; i < 2000; i++)
     buf[i] = _;
   pos = 0;
 }
 
 void scr_print(char *msg, uint8_t color) {
-  uint16_t _ = color << 8;
+  uint16_t _ = (uint16_t)color << 8;
   for (size_t i = 0; msg[i] != 0; i++)
     buf[i] = _ + msg[i];
 }
