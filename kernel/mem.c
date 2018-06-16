cf5fed90e65a3aec14f5d59c309f0c22045d0c80
Benji Dial <Benji3.141@gmail.com>
2018-06-16 19:49:54 -0400

    WIP

--- kernel/mem.c
+++ kernel/mem.c
@@ -5,17 +5,16 @@
 /*Copyright 2018 Benji Dial
   Portland memory management*/
 
-#define MEM_AR_START ((void *)0x2000)
 #define MEM_BLOCK_SIZE ((size_t)32)
-#define MEM_POOL_LEN ((void *)0x8000)
-
-#define MEM_POOL_START (MEM_AR_START + MEM_POOL_LEN * sizeof(bool) / MEM_BLOCK_SIZE)
-#define MEM_POOL_END (MEM_POOL_START + MEM_POOL_LEN)
+#define MEM_POOL_LEN ((size_t)/*TODO*/)
+#define MEM_AR_START ((bool *)/*TODO*/)
+#define MEM_AR_END ((bool *)/*TODO*/)
+#define MEM_POOL_START ((void *)/*TODO*/)
 
 bool *mem_next_ar;
 
 void mem_clear(void) {
-  for (mem_next_ar = MEM_AR_START; mem_next_ar < MEM_POOL_START; mem_next_ar++)
+  for (mem_next_ar = MEM_AR_START; mem_next_ar < MEM_AR_END; mem_next_ar++)
     *mem_next_ar = false;
   mem_next_ar = MEM_AR_START;
 }
@@ -23,7 +22,7 @@ void mem_clear(void) {
 void *mem_alloc_block(void) {
   if (*mem_next_ar) {
     bool *origin = mem_next_ar;
-    while (++mem_next_ar < MEM_POOL_START)
+    while (++mem_next_ar < MEM_AR_END)
       if (!*mem_next_ar)
         goto foundBlock;
     mem_next_ar = MEM_AR_START - 1;
@@ -35,11 +34,11 @@ void *mem_alloc_block(void) {
 
 foundBlock:
   *mem_next_ar = true;
-  void *ret = MEM_POOL_START + MEM_POOL_LEN * (mem_next_ar - MEM_AR_START);
+  void *ret = &MEM_POOL_START[mem_next_ar];
   mem_next_ar++;
   return ret;
 }
 
 void mem_dealloc_block(void *ptr) {
-  *(MEM_AR_START + (ptr - MEM_POOL_START) / MEM_BLOCK_SIZE) = false;
+  MEM_AR_START[(ptr - MEM_POOL_START) / MEM_BLOCK_SIZE] = false;
 }
