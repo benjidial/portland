6324e2bcf3095b189e70e7dfe68bc26226375b1b
Benji Dial <Benji3.141@gmail.com>
2018-06-25 17:25:39 -0400

    Working on keyboard driver, updating shell

--- kernel/kbd.c
+++ kernel/kbd.c
@@ -6,14 +6,49 @@
 /*Copyright 2018 Benji Dial
   Portland PS/2 keyboard driver*/
 
-uint8_t kbd_sc2ch(uint8_t scan) {
-  /*TODO*/
-}
-
+extern uint8_t *kbd_scan_table_0;
+extern uint8_t *kbd_scan_table_1;
 extern uint8_t kbd_gsc(void);
 
 uint8_t kbd_gch(bool print) {
-  uint8_t graph = kbd_sc2ch(kbd_gsc());
+  uint8_t graph;
+  while (1) {
+    uint8_t scan = kbd_gsc();
+    if (scan == 0xf0) {/*Release*/
+      graph = kbd_scan_table_0[kbd_gsc()];
+      break;
+    }
+    else if (scan == 0xe0) {/*Second table*/
+      if ((scan = kbd_gsc()) == 0xf0) {/*Release*/
+        if ((scan = kbd_gsc()) == 0x7c) {
+	  if (kbd_gsc() == 0xe0)
+            if (kbd_gsc() == 0xf0)
+  	      if (kbd_gsc() == 0x12) {
+                graph = /*TODO: Print Screen*/;
+		break;
+	      }
+	}
+	else {
+	  graph = kbd_scan_table_1[scan];
+	  break;
+	}
+      }
+      else if (scan == 0x12)
+        if (kbd_gsc() == 0xe0)
+          kbd_gsc();
+    }
+    else if (scan == 0xe1)
+      if (kbd_gsc() == 0x14)
+        if (kbd_gsc() == 0x77)
+  	  if (kbd_gsc() == 0xe1)
+	    if (kbd_gsc() == 0xf0)
+	      if (kbd_gsc() == 0x14)
+	        if (kbd_gsc() == 0xf0)
+		  if (kbd_gsc() == 0x77) {
+		    graph = /*TODO: Pause*/;
+		    break;
+		  }
+  }
   if (print)
     vga_pch(graph);
   return graph;
