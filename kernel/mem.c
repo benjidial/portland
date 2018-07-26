ba04f524abec1c5dac4384b4d68bb6dc09fb8883
Benji Dial <Benji3.141@gmail.com>
2018-07-26 12:58:07 -0400

    WIP

--- kernel/mem.c
+++ kernel/mem.c
@@ -6,7 +6,7 @@
   Portland memory management*/
 
 #define MEM_POOL_START ((void *)0x4000)
-#define MEM_POOL_END ((void *)(0xffff + 1))
+#define MEM_POOL_END ((void *)(0xffff))
 
 struct mem_record {
   void *start;
@@ -15,12 +15,30 @@ struct mem_record {
   bool allocated;
 }
 
+#define MEM_RECORDS_PER_LINK 10
+
+struct mem_record_link {
+  struct mem_record records[MEM_RECORDS_PER_LINK];
+  struct mem_record_link *next;
+}
+
 struct mem_record *mem_next;
+struct mem_record *mem_last;
+uint16_t mem_n_records;
+struct mem_record_link mem_chain_start;
+struct mem_record_link *mem_chain_end;
+
+struct mem_record *mem_alloc_record(void) {
+  struct mem_record *ret = &(mem_chain_end->records[mem_n_records % MEM_RECORDS_PER_LINK]);
+  if (!(++mem_n_records % MEM_RECORDS_PER_LINK))
+    mem_chain_end = mem_chain_end->next = malloc(sizeof(mem_record_link));
+  return ret;
+}
 
 inline void mem_init(void) {
-  mem_next = /*TODO*/;
+  mem_last = mem_next = mem_alloc_record();
   mem_next->start = MEM_POOL_START;
-  mem_next->length = MEM_POOL_END - MEM_POOL_START;
+  mem_next->length = MEM_POOL_END - MEM_POOL_START + 1;
   mem_next->next = mem_next;
   mem_next->allocated = false;
 }
@@ -37,7 +55,13 @@ inline bool mem_find(uint16_t length) {
 
 void *mem_alloc(uint16_t length) {
   if (!mem_find(length)) {
-    /*TODO: Consolidate adjacent records*/
+    struct mem_record_link *link = &mem_chain_start;
+    uint16_t i = 0;
+    while ((i += MEM_RECORDS_PER_LINK) <= mem_n_records) {
+      for (int j = 0; j < MEM_RECORDS_PER_LINK; j++) {
+        void *end = link->records[j].start + 
+      }
+    }
     if (!mem_find(length))
       return NULL;
   }
@@ -52,7 +76,7 @@ void *mem_alloc(uint16_t length) {
     struct mem_record *old_next = mem_next->next;
     uint16_t old_length = mem_next->length;
     mem_next->length = length;
-    struct mem_record *new = /*TODO*/;
+    struct mem_record *new = mem_alloc_record();
     new->start = mem_next->start + length;
     new->length = old_length - length;
     new->next = old_next;
